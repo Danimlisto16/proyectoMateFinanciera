8164a29f6a1ac4f28f21fd966d1f67db230d6b51
Danimlisto16 <50421224+Danimlisto16@users.noreply.github.com>
2021-03-29 22:50:03 -0500

    cambios para utilizar tasa directamente de los bancos
    se ha implementado una nueva interfaz en un archivo "nuevo.html" para implementar la lógica de credito (amortizacion) y polizas (capitalizacion)

--- logicaAmortizacion.js
+++ logicaAmortizacion.js
@@ -6,32 +6,71 @@ function obtenerValorCampo(campo){
         if(!Number.isNaN(valor))
             if(valor > 0)
                 return parseFloat(valor);
-        alert("El número ingresado no es válido o es menor a 0");
+        //alert("El número ingresado no es válido o es menor a 0");
         $(campo).css('border-color', 'red'); //si el número es NaN pinta de rojo el cuadro
         return -1;
 }
 
+
+function validarCampo(campo){
+    valor = obtenerValorCampo(campo);
+}
+
+function convertirTasa(){
+    tasa = obtenerValorCampo("#inpTasa")
+    if( tasa != -1 ){
+        if(tasa > 0 && tasa <= 1 ){
+            
+            tasaEfectiva = tasa/12;
+            tasaEfectiva = tasaEfectiva.toFixed(3)
+            $("#inpTasaEfectiva").val(tasaEfectiva);
+        }else{
+            $("#inpTasa").css('border-color', 'red'); //si el número es NaN pinta de rojo el cuadro
+        }
+    }else{
+        $("#inpTasaEfectiva").val(0);
+    }
+}
+
+
+function mostrarAbono(){
+
+}
+
+
+
 function calcularAbono( ){
     //Obtener valores
-    monto = obtenerValorCampo("#inpMonto")
-    tasa = obtenerValorCampo("#inpTasa");
+    monto = obtenerValorCampo("#inpMonto");
+    tasa = obtenerValorCampo("#inpTasaEfectiva");
     pagos = obtenerValorCampo("#inpCantPagos");
-    if ($("#tipo_tabla2").is(':checked')) {
-        abono = (monto * tasa)/( 1 - (1 + tasa )**( -pagos));
-        abono = abono.toFixed(3);
-        //console.log(abono)
-        $("#inpAbono").text(abono);
-        $("#tblAmortizacion").empty();
-        calcularTablaAmortizacion(monto ,tasa ,pagos ,abono);    
-    }else if($("#tipo_tabla1").is(':checked')){
-        calcularAbonoCap( );
-    }
 
-    
+    if(monto != -1 && tasa != -1 && pagos != -1){
+        if ($("#tipo_tabla2").is(':checked')) {
+            abono = (monto * tasa)/( 1 - (1 + tasa )**( -pagos));
+            abono = abono.toFixed(3);
+            //console.log(abono)
+            $("#tblAmortizacion").empty();
+            $("#inpAbono").val(abono);
+            $("#lblSaldoMonto").text("Monto");
+            calcularTablaAmortizacion(monto ,tasa ,pagos ,abono);    
+            
+        }else 
+            if($("#tipo_tabla1").is(':checked')){
+                $("#lblSaldoMonto").text("Saldo insoluto");
+                calcularAbonoCap( );
+            }
+    }else{
+        alert("Por favor corrige los datos marcados en rojo antes de calcular");
+        $("#tblAmortizacion").empty(); //limpiar tabla
+    } 
 }
 
 
 function anadirFilasTabla(periodo,amortizacion,intereses,abono,saldoInsoluto){
+    //intereses = intereses.toFixed(3); //aproxima 3 decimales
+    //amortizacion = amortizacion.toFixed(3);
+    //saldoInsoluto = saldoInsoluto.toFixed(3);
     var fila = "<tr> <td> "+periodo+"</td> <td> $"+amortizacion+"</td> <td> $"+intereses+"</td> <td> $"+abono+"</td> <td> $"+saldoInsoluto+"</td> </tr>";
     $("#tblAmortizacion").append(fila);
 }
@@ -42,9 +81,20 @@ function limpiarDatos(campo){
 
 function limparTodo(){
     limpiarDatos("#inpMonto");
+    $("#inpMonto").css('border-color', 'black');
+
     limpiarDatos("#inpTasa");
+    $("#inpTasa").css('border-color', 'black');
+
+    limpiarDatos("#inpTasaEfectiva");
+    //$("#inpTasaEfectiva").css('border-color', 'black');
+
+    limpiarDatos("#inpAbono");
+    $("#inpAbono").css('border-color', 'black');
+
     limpiarDatos("#inpCantPagos");
-    limpiarDatos("#lblAbono");
+    $("#inpCantPagos").css('border-color', 'black');
+    //limpiarDatos("#lblAbono");
     $("#tblAmortizacion").empty(); //limpiar tabla
 }
 
@@ -60,15 +110,13 @@ function calcularTablaAmortizacion(monto ,tasa ,pagos ,abono){
         //calcular valores
         for(i = 1; i <= pagos; i++){
             periodo = i;
+            intereses = tasa*saldoInsoluto;           
+            amortizacion = abono - intereses;            
+            saldoInsoluto = saldoInsoluto - amortizacion; 
 
-            intereses = tasa*saldoInsoluto;
             intereses = intereses.toFixed(3); //aproxima 3 decimales
-
-            amortizacion = abono - intereses;
-            amortizacion = amortizacion.toFixed(3);
-
-            saldoInsoluto = saldoInsoluto - amortizacion;
             saldoInsoluto = saldoInsoluto.toFixed(3);
+            amortizacion = amortizacion.toFixed(3);  
 
             anadirFilasTabla(periodo,amortizacion,intereses,abono,saldoInsoluto);
         }
--- logicaCapitalizacion.js
+++ logicaCapitalizacion.js
@@ -1,15 +1,17 @@
 function calcularAbonoCap( ){
     monto = obtenerValorCampo("#inpMonto")
-    tasa = obtenerValorCampo("#inpTasa");
+    tasa = obtenerValorCampo("#inpTasaEfectiva");
     pagos = obtenerValorCampo("#inpCantPagos");
 
     abono = (monto * tasa)/( (1 + tasa )**( pagos) - 1);
     abono = abono.toFixed(3);
     //console.log(abono)
-    $("#inpAbono").text(abono);
+    
     $("#tblAmortizacion").empty();
+    $("#inpAbono").val(abono);
     calcularTablaCapitalizacion(monto ,tasa ,pagos ,abono);
 }
+
 
 
 function calcularTablaCapitalizacion(monto ,tasa ,pagos ,abono){
@@ -24,12 +26,14 @@ function calcularTablaCapitalizacion(monto ,tasa ,pagos ,abono){
     for(i = 2; i <= pagos; i++){
         periodo = i; 
         amortizacion = saldoInsoluto;
-        amortizacion = parseFloat(amortizacion).toFixed(3);
-        intereses = tasa*amortizacion;
+        
+        intereses = tasa * amortizacion;
         
         saldoInsoluto = parseFloat(amortizacion) + parseFloat(intereses) + parseFloat(abono);
+        
         intereses = intereses.toFixed(3); //aproxima 3 decimales
         saldoInsoluto = saldoInsoluto.toFixed(3);
+        amortizacion = parseFloat(amortizacion).toFixed(3);
         
         anadirFilasTabla(periodo,amortizacion,intereses,abono,saldoInsoluto)
     }
