66d95f4b45cebb16c5dcb1117d2739369d6424f4
mattgr99 <jmgranja1@espe.edu.ec>
2021-03-31 17:25:40 -0500

    Arreglo errores Calculo saldo

--- logicaCapitalizacion.js
+++ logicaCapitalizacion.js
@@ -3,8 +3,8 @@ function calcularAbonoCap( ){
     tasa = obtenerValorCampo("#inpTasaEfectiva");
     pagos = obtenerValorCampo("#inpCantPagos");
 
-    abono = (monto * tasa)/( (1 + tasa )**( pagos) - 1)*(1+tasa);
-    abono = abono.toFixed(3);
+    abono = (monto * tasa)/(( (1 + tasa )**( pagos) - 1)* (1 + tasa));;
+    abono = abono.toFixed(2);
     //console.log(abono)
     
     $("#tblAmortizacion").empty();
@@ -18,22 +18,23 @@ function calcularTablaCapitalizacion(monto ,tasa ,pagos ,abono){
     //agregar primera fila a la matriz 0 0 0 0 monto ganado
     periodo = 1;
     amortizacion = abono;
-    intereses = amortizacion*tasa;
-    saldoInsoluto = amortizacion + intereses;
+    intereses = parseFloat(amortizacion*tasa).toFixed(2);
+    saldoInsoluto = parseFloat(amortizacion) + parseFloat(intereses);
+    saldoInsoluto = parseFloat(saldoInsoluto).toFixed(2);
     anadirFilasTabla(periodo,amortizacion,intereses,abono,saldoInsoluto);
 
     //calcular valores
     for(i = 2; i <= pagos; i++){
         periodo = i; 
-        amortizacion = saldoInsoluto + abono;
+        amortizacion = parseFloat(abono) + parseFloat(saldoInsoluto);
         
-        intereses = tasa * amortizacion;
+        intereses = amortizacion*tasa;
         
         saldoInsoluto = parseFloat(amortizacion) + parseFloat(intereses);
         
-        intereses = intereses.toFixed(3); //aproxima 3 decimales
-        saldoInsoluto = saldoInsoluto.toFixed(3);
-        amortizacion = parseFloat(amortizacion).toFixed(3);
+        intereses = intereses.toFixed(2); //aproxima 3 decimales
+        saldoInsoluto = saldoInsoluto.toFixed(2);
+        amortizacion = parseFloat(amortizacion).toFixed(2);
         
         anadirFilasTabla(periodo,amortizacion,intereses,abono,saldoInsoluto)
     }
