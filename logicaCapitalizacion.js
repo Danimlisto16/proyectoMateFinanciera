9406af61d3e90ccf9136c1a04c0922e63882d979
mattgr99 <jmgranja1@espe.edu.ec>
2021-03-31 14:41:52 -0500

    Capitalizacion con anualidad anticipada

--- logicaCapitalizacion.js
+++ logicaCapitalizacion.js
@@ -3,7 +3,7 @@ function calcularAbonoCap( ){
     tasa = obtenerValorCampo("#inpTasaEfectiva");
     pagos = obtenerValorCampo("#inpCantPagos");
 
-    abono = (monto * tasa)/( (1 + tasa )**( pagos) - 1);
+    abono = (monto * tasa)/( (1 + tasa )**( pagos) - 1)*(1+tasa);
     abono = abono.toFixed(3);
     //console.log(abono)
     
@@ -17,19 +17,19 @@ function calcularAbonoCap( ){
 function calcularTablaCapitalizacion(monto ,tasa ,pagos ,abono){
     //agregar primera fila a la matriz 0 0 0 0 monto ganado
     periodo = 1;
-    amortizacion = 0;
-    intereses = 0;
-    saldoInsoluto = abono;
+    amortizacion = abono;
+    intereses = amortizacion*tasa;
+    saldoInsoluto = amortizacion + intereses;
     anadirFilasTabla(periodo,amortizacion,intereses,abono,saldoInsoluto);
 
     //calcular valores
     for(i = 2; i <= pagos; i++){
         periodo = i; 
-        amortizacion = saldoInsoluto;
+        amortizacion = saldoInsoluto + abono;
         
         intereses = tasa * amortizacion;
         
-        saldoInsoluto = parseFloat(amortizacion) + parseFloat(intereses) + parseFloat(abono);
+        saldoInsoluto = parseFloat(amortizacion) + parseFloat(intereses);
         
         intereses = intereses.toFixed(3); //aproxima 3 decimales
         saldoInsoluto = saldoInsoluto.toFixed(3);
